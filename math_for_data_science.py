3ee5d348267adffeea384e973a2d152c1ec695d5
AnicetJonhia <anicet22.aps2a@gmail.com>
2025-06-03 14:19:03 +0300

    matrice Ax = b

--- math_for_data_science.py
+++ math_for_data_science.py
@@ -1,7 +1,19 @@
 
 
 import numpy as np
-X = np.array([[1, 2, 3],
-              [4, 5, 6]])
+# X = np.array([[1, 2, 3],
+#               [4, 5, 6], 
+#               [7,8,9]])
 
-print(X)
+# print(X)
+
+# det_X = np.linalg.det(X)
+# print(det_X)
+
+
+# Résoudre Ax = b
+A = np.array([[2, 1], [1, 3]])
+b = np.array([8, 13])
+x = np.linalg.solve(A, b)
+
+print(x)
